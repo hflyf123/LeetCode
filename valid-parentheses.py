0f9183ad057ffe2a112f1bf178d28e1fff82da06
lyf <hflyf123@qq.com>
2019-06-01 23:01:07 +0800

    有效的括号

---
+++ valid-parentheses.py
@@ -0,0 +1,45 @@
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+#
+# 有效字符串需满足：
+#
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+# 注意空字符串可被认为是有效字符串。
+#
+# 示例 1:
+#
+# 输入: "()"
+# 输出: true
+# 示例 2:
+#
+# 输入: "()[]{}"
+# 输出: true
+# 示例 3:
+#
+# 输入: "(]"
+# 输出: false
+# 示例 4:
+#
+# 输入: "([)]"
+# 输出: false
+# 示例 5:
+#
+# 输入: "{[]}"
+# 输出: true
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        paren_map = {')': '(', ']': '[', '}': '{'}
+        for c in s:
+            if c not in paren_map:
+                stack.append(c)
+            elif not stack or paren_map[c] != stack.pop():
+                return False
+        return not stack
+
+
+if __name__ == '__main__':
+    soul = Solution()
+    print(soul.isValid("[[(({)})]]"))

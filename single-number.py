8f8a86779ac72f78d0e76eb8a6705d451cba75d1
liyanfeng <liyanfeng@xiaomi.com>
2019-06-04 12:03:17 +0800

    只出现一次的数字

---
+++ single-number.py
@@ -0,0 +1,28 @@
+#
+from typing import List
+
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         buffer_dict = {x: 0 for x in nums}
+#         for x in nums:
+#             buffer_dict[x] += 1
+#         for k, v in buffer_dict.items():
+#             if v == 1:
+#                 return k
+
+# BEST SOLUTION
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        temp = 0
+        for i in nums:
+            temp ^= i
+        return temp
+
+
+
+if __name__ == '__main__':
+    soul = Solution()
+    print(soul.singleNumber([-1,-1,-2]))

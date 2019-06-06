8e05646e7a46005bfe402426f7f694351d2081ad
liyanfeng <liyanfeng@xiaomi.com>
2019-06-06 16:57:25 +0800

    选择排序

--- sort/SelectSort.py
+++ sort/SelectSort.py
@@ -1,6 +1,15 @@
 from typing import List
 
 
-def select_sort(nums:List[int]) -> List[int]:
-    for i in range(len(nums), 0, -1):
-        pass
+def select_sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums)):
+        p_min = i
+        for j in range(i, len(nums)):
+            if nums[j] < nums[p_min]:
+                p_min = j
+        nums[p_min], nums[i] = nums[i], nums[p_min]
+    return nums
+
+
+if __name__ == '__main__':
+    print(select_sort([5, 4, 3, 2, 1]))

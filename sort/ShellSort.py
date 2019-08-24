6a91b512b6076e74888a538321734c981cc926f9
ligang <hflyf123@outlook.com>
2019-08-24 15:53:04 +0800

    希尔排序

--- sort/ShellSort.py
+++ sort/ShellSort.py
@@ -6,9 +6,8 @@
 # 每趟排序，根据对应的增量 ti，将待排序列分割成若干长度为 m 的子序列，分别对各子表进行直接插入排序。仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度
 
 
-
 from typing import List
 
 
-def shell_sort(nums:List[int]) -> List[int]:
+def shell_sort(nums: List[int]) -> List[int]:
     pass

91383190ff5969f5483ad2456ecb5f4702254e6d
ligang <hflyf123@outlook.com>
2019-08-27 09:53:30 +0800

    求众数

--- majority-element.py
+++ majority-element.py
@@ -16,6 +16,7 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # map存储次数, 时间复杂度o(n),空间复杂度o(n)
         cache_dict = {}
         for i in nums:
             if i not in cache_dict:
@@ -31,9 +32,32 @@ class Solution:
         if cache_dict[key] > len(nums) / 2:
             return key
 
+    # 暴力解法,时间复杂度o(n^2),通过不了
+    def majorityElement1(self, nums: List[int]) -> int:
+        for item in nums:
+            count = nums.count(item)
+            if count > len(nums) / 2:
+                return item
 
-#BEST SOLUTION
+    # sort,判断重复次数,大于n/2的就是解,o(nlogn)
+    def majorityElement2(self, nums: List[int]) -> int:
+        sorted_nums = nums.sort()
+        times = 1
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                times += 1
+            else:
+                times = 1
+            if times > len(nums)/2:
+                return nums[i]
+        # 如果只有一個數,那么第一个数就是众数
+        return nums[times]
 
+    # 分治做法,如果两边的majority相等,就得出了结果,若是不相同,则比较两边出现次数多的majority
+    def majorityElement3(self, nums: List[int]) -> int:
+        pass
+
+# BEST SOLUTION
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
 #         result = {}
@@ -43,5 +67,5 @@ class Solution:
 #             if value > len(nums)/2:
 #                 return key
 if __name__ == '__main__':
-   soul = Solution()
-   print(soul.majorityElement([2,2,1,1,1,2,2]))
+    soul = Solution()
+    print(soul.majorityElement2([1]))
